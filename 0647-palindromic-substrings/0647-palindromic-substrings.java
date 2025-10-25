055217e6d4e1a5789dcaaac8ad74ddd4549218b7
ginakohli <68778985+ginakohli@users.noreply.github.com>
2025-10-25 21:08:23 +0530

    Time: 6 ms (68.58%), Space: 41.9 MB (42.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,30 +1,24 @@
 class Solution {
+
+    int count = 0;
     public int countSubstrings(String s) {
 
-        int count = 0;
         int n = s.length();
-        boolean dp[][] = new boolean[n+1][n+1];
-      
-        for(int L =1; L <= n; L++){
-            for(int i = 0; i+L-1 < n; i++){
-                int j = i+L-1;
-                if(L==1){
-                   dp[i][j] = true;
-                }
-                else if(L==2){
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                }
-                else{
-                    dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i+1][j-1];
-                }
-
-                if(dp[i][j]){
-                    count++;
-                }
-            }
+        
+        for(int i = 0; i < n; i++){
+          checkPalindrome(i, i, s);
+          checkPalindrome(i, i+1, s);
         }
         return count;
-    }
+      }
 
-    
+
+    public int checkPalindrome(int i, int j, String s){
+          while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            count++;
+            i--; //expand left
+            j++; // expand right
+    }
+    return count;
+    }
 }
