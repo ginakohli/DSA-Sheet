53f2b3126d1fe4df2938c44b07fe64f62f9ec952
ginakohli <68778985+ginakohli@users.noreply.github.com>
2025-10-28 00:21:03 +0530

    Time: 223 ms (21.04%), Space: 58.7 MB (5.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,9 +4,10 @@ class Solution {
               int n = s.length();
               int idx = 0;
               int maxLen = 1;
+              Boolean[][] dp = new Boolean[n+1][n+1];
               for(int i = 0; i < n; i++){
                   for(int j = i; j < n; j++){
-                    if(checkPalindrome(s, i, j)){
+                    if(checkPalindrome(s, i, j, dp)){
                         if(j-i+1 > maxLen){
                         maxLen = Math.max(maxLen,j-i+1);
                         idx = i;
@@ -17,13 +18,16 @@ class Solution {
         return s.substring(idx, idx + maxLen);
     }
 
-    public boolean checkPalindrome(String s, int i, int j){
+    public boolean checkPalindrome(String s, int i, int j, Boolean[][] dp){
         if(i >= j){
             return true;
 
         }
+        else if(dp[i][j] != null){
+            return dp[i][j];
+        }
         else if(s.charAt(i) == s.charAt(j)){
-            return checkPalindrome(s, i+1, j-1);
+            return dp[i][j] = checkPalindrome(s, i+1, j-1, dp);
         }
         else{
             return false;
