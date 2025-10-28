618fbec1666daab1bcbc9a6c946c140288365e04
ginakohli <68778985+ginakohli@users.noreply.github.com>
2025-10-28 10:45:24 +0530

    Time: 12 ms (7.19%), Space: 45 MB (5.3%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -21,21 +21,41 @@ class Solution {
 
 
      //vertical scanning
-     if(strs.length == 0 || strs == null){
-            return "";
-        }
+//      if(strs.length == 0 || strs == null){
+//             return "";
+//         }
     
 
-        int len = strs[0].length();
-        for(int i = 0; i < len; i++){
-            Character c = strs[0].charAt(i);
-            for(int j = 1; j < strs.length; j++){
-                if(i >= strs[j].length() || strs[j].charAt(i)!=c){
-                    return strs[j].substring(0, i);
+//         int len = strs[0].length();
+//         for(int i = 0; i < len; i++){
+//             Character c = strs[0].charAt(i);
+//             for(int j = 1; j < strs.length; j++){
+//                 if(i >= strs[j].length() || strs[j].charAt(i)!=c){
+//                     return strs[j].substring(0, i);
+//                 }
+//             }
+//         }
+//         return strs[0];
+//     }
+
+    int n = strs.length;
+        String lcp = strs[0];
+        String cp = "";
+        for(int i = 1; i < n; i++){                                                                                        
+             String s = strs[i];
+             cp = "";
+             for(int j = 0; j < Math.min(s.length(), lcp.length()); j++){
+                
+                if(lcp.charAt(j) == s.charAt(j)){
+                      cp += lcp.charAt(j);
                 }
-            }
+                else{
+                    break; 
+                }
+             }
+             lcp = cp;
         }
-        return strs[0];
+         return lcp;
     }
 }
 
