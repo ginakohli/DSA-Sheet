27c67ed3d4df92e43c722e599ea952a7741254b5
ginakohli <68778985+ginakohli@users.noreply.github.com>
2025-12-20 08:18:26 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.68%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hammingWeight(int n) {
+
+        //appproach 1 , check ith bit if its 1 , increase count
+        int count = 0;
+       for(int i = 31; i>=0; i--){
+        if(((n >> i) & 1) == 1){
+             count++;
+        }
+        
+       }
+       return count;
+    }
+}
+
+//approach 1
+//count each bit and unset it to 0 -> n & (n-1)
+
+
+//approach 2
+//take binary of n till n !=0
+
+//approach 3
+//Take Integer.BitCount();
+
+//approach 4
+//
