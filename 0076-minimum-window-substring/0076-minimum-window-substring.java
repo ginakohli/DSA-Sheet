273d4779356a691510217ff888eaf0dba954105a
ginakohli <68778985+ginakohli@users.noreply.github.com>
2026-01-22 07:53:58 +0530

    Time: 17 ms (55.9%), Space: 46.9 MB (61.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,49 +2,52 @@ class Solution {
     public String minWindow(String s, String t) {
 
         Map<Character, Integer> freq = new HashMap<>();
-        for(int i = 0; i < t.length(); i++){
-            freq.put(t.charAt(i), freq.getOrDefault(t.charAt(i),0)+1);
+        for(Character c:t.toCharArray()){
+            freq.put(c, freq.getOrDefault(c,0)+1);
         }
-        int count = freq.size();
-        int i = 0;
-        int j = 0;
-        int low = 0;
-        int high = 0;
-        int minLength = Integer.MAX_VALUE;
-        boolean flag = false;
-        while( j < s.length()){
 
+         int i=0;int j = 0;
+         int low = 0;
+         int high = 0;
+         int minLen = Integer.MAX_VALUE;
+         int n = s.length();
+         int count = freq.size();
+         boolean flag = false;
+         while(j < n){
+               
             if(freq.containsKey(s.charAt(j))){
                 freq.put(s.charAt(j), freq.get(s.charAt(j))-1);
-                if(freq.get(s.charAt(j))==0){
-                    count--;
-                }
-            }
-            j++;
-            
-            while(i < s.length() && count == 0){
-                
-              if(freq.containsKey(s.charAt(i))){
-                freq.put(s.charAt(i), freq.get(s.charAt(i))+1);
-                if(freq.get(s.charAt(i))>0){
-                    count++;
-                }
-            }
-            
-            
-            if(minLength > j-i+1){
-                minLength = j-i+1;
-                low = i;
-                high = j;
-                flag = true;
-            }
-           i++;
-        }
-        
+
+                     if(freq.get(s.charAt(j))==0){
+                         count--;
+                      }
+               }
+
+               j++;
+
+               while(i < n && count == 0){
+                   if(freq.containsKey(s.charAt(i))){
+                       freq.put(s.charAt(i), freq.get(s.charAt(i))+1);
+
+                       if(freq.get(s.charAt(i))>0
+                       ){
+                              count++;
+                       }
+                   }
+
+                   if(minLen > j-i+1){
+                       minLen = j-i+1;
+                       low = i;
+                       high = j;
+                    flag = true;
+                   }
+                   i++;
+               }
+         }
+
+         if(flag == false){
+            return "";
+         }
+        return s.substring(low, high);
     }
-    if(flag == false){
-        return "";
-    }
-    return s.substring(low, high);
-}
 }
