901fa6b25a8d9f2293151325025579263dd5a988
ginakohli <68778985+ginakohli@users.noreply.github.com>
2026-01-22 15:24:48 +0530

    Time: 26 ms (37.36%), Space: 48.1 MB (10.53%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,48 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String t) {
-
-        int i = 0;
-        int j = 0;
-        int count = 0;
-        int n = s.length();
-        List<Integer> result = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-
-        if(t.length() > s.length()){
-            return result;
-        }
-        for(Character c : t.toCharArray()){
-            map.put(c, map.getOrDefault(c,0)+1);
-        }
-      
-         count  = map.size();
-
-         while(j < n){
-
-            if(map.containsKey(s.charAt(j))){
-                map.put(s.charAt(j), map.get(s.charAt(j))-1);
-                if(map.get(s.charAt(j))==0){
-                    count --;
-                }
+            List<Integer> result = new ArrayList<>();
+            int n = s.length();
+            Map<Character, Integer> map = new HashMap<>();
+            for(Character c : t.toCharArray()){
+                map.put(c, map.getOrDefault(c,0)+1);
             }
-           j++;
-            while(count == 0){
-                if(j-i == t.length()){
-                    result.add(i);
-                }
 
-                if(map.containsKey(s.charAt(i))){
-                    map.put(s.charAt(i), map.get(s.charAt(i))+1);
-                    if(map.get(s.charAt(i))>0){
-                        count++;
+            int i = 0;
+            int j = 0;
+            int count = map.size();
+            while(j < n){
+                if(map.containsKey(s.charAt(j))){
+                    map.put(s.charAt(j), map.get(s.charAt(j))-1);
+                    if(map.get(s.charAt(j))==0){
+                         count--;
                     }
                 }
-                i++;
+                j++;
+
+                while(count == 0){
+
+                    if(j-i == t.length()){
+                        result.add(i);
+                    }
+                    if(map.containsKey(s.charAt(i))){
+                        map.put(s.charAt(i), map.get(s.charAt(i))+1);
+                        if(map.get(s.charAt(i))>0){
+                            count++;
+                        }
+                    }
+
+                    i++;
+                }
             }
-
-        }
-
-        return result;
-  
+       return result;
     }
 }
